5cd95959c264a1482a8a2cee82d9db8618603614
Santiago Aramburu <santiburu77@gmail.com>
2025-08-19 16:41:46 -0400

    Working on Capture Function

--- app.js
+++ app.js
@@ -1,56 +1,97 @@
-const originalBodyHtml = document.body.innerHTML; // Store the original HTML of the body
 const boardEl = document.querySelector(".board");
-const piecesEl = document.querySelectorAll(".piece");
+const whitePiecesEl = document.querySelectorAll(".white");
+const blackPiecesEl = document.querySelectorAll(".black");
 const displayEl = document.querySelector(".display");
 const buttonEl = document.querySelector(".button");
+const knightEl = document.querySelectorAll(".knight");
 
-let selectedSquare = null;
+[...whitePiecesEl, ...blackPiecesEl].forEach((el) => {
+  el.addEventListener("click", (e) => {
+    // TODO: Make sure to remove this event listener from any other elements once you're done with turn
+    e.target.style.cursor = "grabbing";
+  });
+});
+
+let selectedPiece = null;
 let currentPlayer = "white"; // Track the current player (white or black)
+let targetSquare = null;
 
 const handlePlayer = () => {
-  currentPlayer = currentPlayer === "white" ? "black" : "white"; // Switch turns between players
+  // Switch turns between players
+  currentPlayer = currentPlayer === "white" ? "black" : "white";
   displayMessage(`It's ${currentPlayer}'s turn`);
   console.log("Current player:", currentPlayer);
 };
 
 const clickedHandler = (e) => {
-  e.target.style.border = "blue 1px solid"; // This will add a blue border to the clicked element.
-  if (selectedSquare) {
-    selectedSquare.style.border = "none"; // Remove border from previously selected square
+  if (selectedPiece) {
+    selectedPiece.style.border = "none"; // Remove border from previously selected square
   }
-  selectedSquare = e.target; // Update the selected square to the currently clicked one
-  console.log("Selected square:", selectedSquare);
+
+  if (e.target.classList.contains(currentPlayer)) {
+    selectedPiece = e.target; // Update the selected square to the currently clicked one
+    e.target.style.border = "blue 1px solid"; // This will add a blue border to the clicked element.
+  }
+
+  if (currentPlayer === "white" && e.target.classList.contains("black")) {
+    //White Captures black
+    e.target.classList = selectedPiece.classList;
+    e.target.textContent = "";
+    e.target.appendChild(selectedPiece);
+    console.log("capture", e.target);
+    handlePlayer();
+  } else if (
+    currentPlayer === "black" &&
+    e.target.classList.contains("white")
+  ) {
+    e.target.classList = selectedPiece.classList;
+    e.target.textContent = "";
+    e.target.appendChild(selectedPiece);
+    console.log("capture", e.target);
+    handlePlayer();
+  }
 };
 
 const displayMessage = (message) => {
   //This will display messages to user, who's turn is it, who won, tie.
+
   displayEl.textContent = message;
   console.log(message);
 };
 
-piecesEl.forEach((piece) => {
-  //Adding event listener to each piece
-  //selecting all the pieces in the board
+whitePiecesEl.forEach((piece) => {
+  //Adding event listener to each piece, selecting all the pieces in the board
+  piece.addEventListener("click", clickedHandler);
+});
+
+blackPiecesEl.forEach((piece) => {
+  //Adding event listener to each piece selecting all the pieces in the board
   piece.addEventListener("click", clickedHandler);
 });
 
 boardEl.addEventListener("click", (e) => {
+  //Moves piece
   // Add event listener to the board
-  if (selectedSquare && e.target.classList.contains("square")) {
+  if (selectedPiece && e.target.classList.contains("square")) {
     // Ensure a piece is selected and the clicked target is a square
-    e.target.appendChild(selectedSquare); // Move the selected piece to the clicked square
-    selectedSquare.style.border = "none"; // Remove border after moving
-    selectedSquare = null; // Reset selected square
+    e.target.appendChild(selectedPiece); // Move the selected piece to the clicked square
+    selectedPiece.style.border = "none"; // Remove border after moving
+    selectedPiece = null; // Reset selected square
+    console.log("target square:", e.target.id);
     handlePlayer();
+  }
+
+  if (
+    e.target.classList.contains("square") ||
+    e.target.childNodes.length !== 0
+  ) {
+    console.log("BoardSquare", e.target.id || e.target.parentNode.id);
   }
 });
 
 buttonEl.addEventListener("click", () => {
-  console.log("Resetting the game...");
-  document.body.innerHTML = originalBodyHtml;
-  currentPlayer = "white"; // Reset to white player
-  console.log("Current player:", currentPlayer);
-  displayMessage(`It's ${currentPlayer}'s turn`);
+  // Reset the game state
+  window.location.reload(true);
 });
-selectedSquare = null; // Reset selected square
-// }); // Reset the game when the button is clicked
+
+// Each piece
